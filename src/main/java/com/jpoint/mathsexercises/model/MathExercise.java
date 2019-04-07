2b5607994fc53cfd9dd313cdfdf2e64ea2dafa97
Sergey Vorobyev <sergey.vorobyov.sparrow@gmail.com>
2019-04-08 01:13:44 +0300

    refactoring: packages and model

---
+++ src/main/java/com/jpoint/mathsexercises/model/MathExercise.java
@@ -0,0 +1,9 @@
+package com.jpoint.mathsexercises.model;
+
+import lombok.Data;
+
+@Data
+public class MathExercise {
+    private final String question;
+    private final String answer;
+}
--- src/main/java/com/jpoint/mathsexercises/model/MathRest.java
+++ src/main/java/com/jpoint/mathsexercises/model/MathRest.java
@@ -1,6 +1,5 @@
-package com.jpoint.mathsexercises;
+package com.jpoint.mathsexercises.model;
 
-import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,12 +19,12 @@ public class MathRest {
     private final Random random = new Random();
 
     @GetMapping("/{q}")
-    public List<Exercise> getExercises(@PathVariable int q,
-                                       @RequestParam(required = false, name = "level", defaultValue = "100") int level) {
+    public List<MathExercise> getExercises(@PathVariable int q,
+                                           @RequestParam(required = false, name = "level", defaultValue = "100") int level) {
         return generateExercises(q, level);
     }
 
-    private List<Exercise> generateExercises(int q, int level) {
+    private List<MathExercise> generateExercises(int q, int level) {
         return IntStream.range(0, q)
                 .mapToObj(i -> {
                     final Character operation = odd(i) ? '+' : '*';
@@ -35,14 +34,8 @@ public class MathRest {
                     final long answer = operation.equals('+') ? a + b : a * b;
                     final String question = valueOf(a) + operation + b;
 
-                    return new Exercise(question, valueOf(answer));
+                    return new MathExercise(question, valueOf(answer));
                 }).collect(toList());
-    }
-
-    @Data
-    private class Exercise {
-        private final String question;
-        private final String answer;
     }
 
     private static boolean odd(int num) {
